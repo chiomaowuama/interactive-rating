a647ddda976e7ff867e847fd3ecbb237187e4069
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-05-07 21:57:03 +0100

    found an error and is fixing iit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const pElements = document.getElementsByClassName('two');
-const btnEle = document.getElementById('myButton')
+const btnEle = document.getElementById('myButton');
+let chosenNum = document.getElementById('choosen-numbers');
 let numEle = null
-
+let returnednumber = null
 
 function handleClick(event) {
     const pElement = event.target;
@@ -11,9 +12,8 @@ function handleClick(event) {
 for (let i = 0; i < pElements.length; i++) {
     const pElement = pElements[i];
     pElement.addEventListener('click', handleClick);  
+    btnEle.addEventListener('click', saveNum);
 }
-btnEle.addEventListener('click', saveNum)
-
 function saveNum(){
     if(!numEle){
         console.log("sometthing is missing")
@@ -21,8 +21,13 @@ function saveNum(){
     else{
         // console.log(numEle)
         localStorage.setItem('theNumber', numEle);
-       let returnednumber = localStorage.getItem('theNumber')
-       console.log(returnednumber)
+        returnednumber = localStorage.getItem('theNumber')
         // window.location.assign('thankyou.html')
     }
 }
+// chosenNum.addEventListener('DOMContentLoaded', displayNum)
+function displayNum(){
+    console.log("yes")
+  
+}
+
