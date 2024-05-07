1249641062904aab470d1a856b0f66bda6274664
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-05-07 19:12:03 +0100

    button working

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 
 const pElements = document.getElementsByClassName('two');
+const btnEle = document.getElementById('myButton')
 
 
 function handleClick(event) {
     const pElement = event.target;
     const theAttri = pElement.getAttribute('value')
-    console.log(theAttri);
+    console.log(theAttri)
+    // if(!theAttri){
+    //     alert('please rate us')
+    // }else{
+
+    //     saveNum(theAttri)
+    // }
 }
 
 
 for (let i = 0; i < pElements.length; i++) {
     const pElement = pElements[i];
     pElement.addEventListener('click', handleClick);
+    
+   
+}
+btnEle.addEventListener('click', saveNum)
+
+function saveNum(theAttri){
+    console.log(theAttri)
 }
